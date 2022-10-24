3bfef5589922d3f85166c4adb3d34eea44247cda
Akshay Mukul <94642873+Mukul65@users.noreply.github.com>
2022-10-24 16:17:18 +0530

    Update Population.cpp
    Resolved issue

--- Population.cpp
+++ Population.cpp
@@ -1,3 +1,17 @@
+//Problem name - Population
+/*If there were initially XX million people in a town, out of which YY million people left the town and ZZ million people immigrated to this town.
+
+Determine the final population of town in millions.
+
+Input Format
+
+-The first line of input will contain a single integer TT, denoting the number of test cases.
+-The first and only line of each test case consists of three integers XX, YY and ZZ.
+Output Format
+
+-For each test case, output the final population of the town in millions.*/
+
+
 #include <iostream>
 using namespace std;
 
