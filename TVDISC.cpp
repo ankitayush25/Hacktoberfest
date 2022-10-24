92f6a741403265cfe051beaaf5c940018a9c408c
Ezra <71969300+ankitayush25@users.noreply.github.com>
2022-10-24 12:44:01 +0530

    Create TVDISC.cpp

---
+++ TVDISC.cpp
@@ -0,0 +1,26 @@
+*/
+ Problem name : TVDISC
+
+Chef is looking to buy a TV and has shortlisted two models. The first one costs AA rupees, while the second one costs BB rupees.
+
+Since there is a huge sale coming up on Chefzon, Chef can get a flat discount of CC rupees on the first TV, and a flat discount of DD rupees on the second one.
+
+Help Chef determine which of the two TVs would be cheaper to buy during the sale.
+Input Format
+
+    The first line contains a single integer TT — the number of test cases. Then the test cases follow.
+    The first and only line of each test case contains four space-separated integers AA, BB, CC and DD — the marked price (in rupees) of the first TV, the marked price (in rupees) of the second TV, the flat discount (in rupees) of the first TV, and the flat discount (in rupees) of the second TV.
+
+//Output Format
+
+For each test case, print a single line containing the string First if the first TV is cheaper to buy with discount, or Second if the second TV is cheaper to buy with discount. If both of them cost the same after discount, print Any.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings first, First, fIRSt, and FIRST will all be treated as identical).
+Constraints
+
+    1≤T≤50001≤T≤5000
+    1≤A,B≤1001≤A,B≤100
+    0≤C≤A0≤C≤A
+    0≤D≤B0≤D≤B
+
+*/
