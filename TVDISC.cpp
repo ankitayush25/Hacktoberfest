eb7736ef0b93857eb7317a18f631dd230b61e5a5
Akshay Mukul <94642873+Mukul65@users.noreply.github.com>
2022-10-24 14:40:12 +0530

    Update TVDISC.cpp

--- TVDISC.cpp
+++ TVDISC.cpp
@@ -24,3 +24,24 @@ Constraints
     0≤D≤B0≤D≤B
 
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+     int a,b,c,d;
+     cin>>a>>b>>c>>d;
+     
+     if(a-c == b-d)
+     cout<<"Any"<<endl;
+     else if (a-c > b-d)
+     cout<<"Second"<<endl;
+     else
+     cout<<"First"<<endl;
+    }
+	return 0;
+}
