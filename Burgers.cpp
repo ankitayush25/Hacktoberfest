e3b2af20a99c381fb31a76c462db124e4b0e105f
Akshay Mukul <94642873+Mukul65@users.noreply.github.com>
2022-10-24 15:35:31 +0530

    Create Burgers.cpp

---
+++ Burgers.cpp
@@ -0,0 +1,12 @@
+/*Problem
+
+Chef is very hungry. So, Chef goes to a shop selling burgers. The shop has 22 types of burgers:
+
+    Normal burgers, which cost XX rupees each
+    Premium burgers, which cost YY rupees each (where Y>XY>X)
+
+Chef has RR rupees. Chef wants to buy exactly NN burgers. He also wants to maximize the number of premium burgers he buys. Determine the number of burgers of both types Chef must buy.
+
+Output −1−1 if it is not possible for Chef to buy NN burgers.
+*/
+
