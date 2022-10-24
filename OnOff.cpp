ee11d0684f18509797e812be8a581f212a1357ee
Akshay Mukul <94642873+Mukul65@users.noreply.github.com>
2022-10-24 15:17:22 +0530

    Create OnOff.cpp

---
+++ OnOff.cpp
@@ -0,0 +1,11 @@
+
+//Problem - N Buttons 1 Bulb
+//link -https://www.codechef.com/START49D/problems/ON_OFF
+
+//Kulyash stays in room that has a single bulb and NN buttons. The bulb is initially on.
+
+//The initial states of the buttons are stored in a binary string SS of length NN — if $ is 00, the ii-th button is off, and if $ is 11, the ii-th button is on. If Kulyash toggles any single button then the state of the bulb reverses i.e. the bulb lights up if it was off and vice versa.
+
+//Kulyash has toggled some buttons and the final states of the buttons are stored in another binary string RR of length NN. He asks you to determine the final state of the bulb.
+
+
