335bc104ae5db18db95f82c32874a66dbb17cfee
Akshay Mukul <94642873+Mukul65@users.noreply.github.com>
2022-10-24 15:18:21 +0530

    Update OnOff.cpp

--- OnOff.cpp
+++ OnOff.cpp
@@ -9,3 +9,39 @@
 //Kulyash has toggled some buttons and the final states of the buttons are stored in another binary string RR of length NN. He asks you to determine the final state of the bulb.
 
 
+//code-
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int t;
+	cin>>t;
+	while(t--){
+	    
+	    long long int n;
+	    cin>>n;
+	    string a,b;
+	    cin>>a>>b;
+	    long long int c1=0,c2=0;
+	    
+	    for(int i=0;i<n;i++){
+	        if(a[i]=='1')
+	        c1++;
+	    }
+	    for(int i=0;i<n;i++){
+	        
+	        if(b[i]=='1')
+	        c2++;
+	    }
+	    long long int di=abs(c1-c2);
+	    if(di%2==0)
+	    cout<<"1"<<endl;
+	    else
+	    cout<<"0"<<endl;
+	        
+	    }
+	    
+	return 0;
+}
